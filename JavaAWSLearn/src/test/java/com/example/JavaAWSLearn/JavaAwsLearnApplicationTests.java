894a7da14f0866d97f6af8dd13c20d93206fc9e7
Sowmya-Tummala94 <sowmya.tummala94@gmail.com>
2024-03-13 20:12:31 -0400

    Initial commit

---
+++ JavaAWSLearn/src/test/java/com/example/JavaAWSLearn/JavaAwsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaAWSLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
